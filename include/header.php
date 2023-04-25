acb7004a57b839a6c65f3ccc4115ec97b1e03f48
Pawanyy <pawanyyy01@gmail.com>
2023-04-25 20:44:46 +0530

    added box icons header

--- include/header.php
+++ include/header.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <!-- Box Icons -->
-<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<link href='<?=BASE_URL?>assets/plugins/boxicons/css/boxicons.min.css' rel='stylesheet'>
 <!-- Theme style -->
 <link rel="stylesheet" href="<?=BASE_URL?>assets/plugins/bootstrap/css/bootstrap.min.css">
 <!-- <link rel="stylesheet" href="<?=BASE_URL?>assets/plugins/bootstrap/css/bootstrap.superhero.min.css"> -->
